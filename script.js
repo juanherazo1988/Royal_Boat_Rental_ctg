1fe105abab45f49f95f50e0b0b148fea253bb7b7
juanherazo1988@hotmail.com <jhondavid751988@hotmail.com>
2026-03-12 23:27:19 -0500

    ajuste para menu2

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-let index = 0;
-
-mostrarImagen();
-
-function mostrarImagen() {
-    let imagenes = document.getElementsByClassName("slide");
-
-    for (let i = 0; i < imagenes.length; i++) {
-        imagenes[i].style.display = "none";
-    }
-
-    index++;
-
-    if (index >= imagenes.length) {
-        index = 0;
-    }
-
-    imagenes[index].style.display = "block";
-
-    setTimeout(mostrarImagen, 3000);
-}
-
-
-// Función para alternar el menú hamburguesa
 const toggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
-toggle.addEventListener("click", () => {
-  menu.classList.toggle("active");
+toggle.addEventListener("click", function(){
+menu.classList.toggle("active");
 });
+
 
