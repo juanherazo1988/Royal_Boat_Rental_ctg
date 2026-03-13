c35d3f0ae18cc02fb68b8f2a02538ac019dc2445
juanherazo1988@hotmail.com <jhondavid751988@hotmail.com>
2026-03-12 22:39:08 -0500

    ajuste para menu unificado1

--- script.js
+++ script.js
@@ -21,10 +21,11 @@ function mostrarImagen() {
 }
 
 
+// Función para alternar el menú hamburguesa
 const toggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
 toggle.addEventListener("click", () => {
-menu.classList.toggle("active");
+  menu.classList.toggle("active");
 });
 
